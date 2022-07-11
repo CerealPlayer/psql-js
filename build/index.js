249c7d0b7e823bcaf4382a8eb4d2c079e57860cb
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-07-11 21:12:33 +0200

    Prep the field for packaging

--- build/index.js
+++ build/index.js
@@ -1,2 +1,5 @@
 "use strict";
-console.log("hello world");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PSQL = void 0;
+var Client_1 = require("./Client");
+Object.defineProperty(exports, "PSQL", { enumerable: true, get: function () { return Client_1.PSQL; } });
